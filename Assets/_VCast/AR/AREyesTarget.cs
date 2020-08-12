0aa2aaa043881f4a96450369eda8c29bb1b81831
Jaime Hernandez <jaimevazz@gmail.com>
2020-08-12 13:59:46 -0500

    Added EyeTrackingTarget

--- Assets/_VCast/AR/AREyesTarget.cs
+++ Assets/_VCast/AR/AREyesTarget.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 #if UNITY_IOS && !UNITY_EDITOR
@@ -12,16 +10,46 @@ namespace VCast.XR.ARFoundation
     [RequireComponent(typeof(ARFace))]
     public class AREyesTarget : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [SerializeField] Transform Eye = default;
+        [SerializeField] Transform target = default;
+        [SerializeField] float focalDistance = 0.25f;
+        [SerializeField] float targetDistance = 10.0f;
+        [SerializeField] bool mirror = true;
+        ARFace m_Face;
+        XRFaceSubsystem m_FaceSubsystem;
+
+        void Awake()
         {
-            
+            m_Face = GetComponent<ARFace>();
         }
 
-        // Update is called once per frame
-        void Update()
+        void OnEnable()
         {
-            
+            var faceManager = FindObjectOfType<ARFaceManager>();
+            if (faceManager != null && faceManager.subsystem != null && faceManager.descriptor.supportsEyeTracking)
+            {
+                m_FaceSubsystem = (XRFaceSubsystem)faceManager.subsystem;
+                m_Face.updated += OnUpdated;
+            }
+            else
+            {
+                enabled = false;
+            }
+        }        
+        void OnDisable()
+        {
+            m_Face.updated -= OnUpdated;
+        }
+
+        void OnUpdated(ARFaceUpdatedEventArgs eventArgs)
+        {   
+            target.position = Eye.position + m_Face.rightEye.forward * targetDistance;
+            float x  = target.localPosition.x * (mirror ? -1.0f : 1.0f);
+            float y  = -target.localPosition.y;
+            float z  = target.localPosition.z * focalDistance;
+            target.localPosition = new Vector3(x, y, z);
+            Debug.LogFormat("Eye target local position: {0}", target.localPosition);
+            Debug.LogFormat("Eye target position: {0}", target.position);            
         }
     }
 }
