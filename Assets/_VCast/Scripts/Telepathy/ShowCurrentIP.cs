31a1bbf2cde09d0b26504232b36d9a19b5984795
Jaime Hernandez <jaimevazz@gmail.com>
2020-09-19 08:18:01 -0500

    Show server IP script, Sync Server config

---
+++ Assets/_VCast/Scripts/Telepathy/ShowCurrentIP.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using UnityEngine;
+using TMPro;
+
+public class ShowCurrentIP : MonoBehaviour
+{
+    [SerializeField] TMP_Text text = default;
+
+    public string GetLocalIPv4()
+     {
+         return Dns.GetHostEntry(Dns.GetHostName())
+             .AddressList.First(
+                 f => f.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+             .ToString();
+     }    
+
+    void Start()
+    {
+        text.text = GetLocalIPv4();
+    }
+}
