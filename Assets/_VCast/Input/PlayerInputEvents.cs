c570ac984dd57bfb143cb0cf670530366eb8c638
Jaime Hernandez <jaimevazz@gmail.com>
2021-02-01 00:17:40 -0600

    Added InputControls

---
+++ Assets/_VCast/Input/PlayerInputEvents.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputEvents : MonoBehaviour
+{
+    InputControls controls;
+    Vector2 move;
+    CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    Animator animator;
+
+    void Awake ()
+    {
+        controls = new InputControls();
+        controls.Player.Fire.performed += ctx => Kick();
+        controls.Player.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => move = Vector2.zero;
+    }
+
+    void Kick()
+    {
+        Debug.Log("Kicked");
+        animator.SetTrigger("Kick");
+    }
+    // Start is called before the first frame updatebuttonSouth
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    void OnEnable()
+    {
+        controls.Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Player.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+        Vector3 move = new Vector3(this.move.x, 0, this.move.y);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+        // // Changes the height position of the player..
+        // if (Input.GetButtonDown("Jump") && groundedPlayer)
+        // {
+        //     playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        // }
+
+        // playerVelocity.y += gravityValue * Time.deltaTime;
+        // controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
